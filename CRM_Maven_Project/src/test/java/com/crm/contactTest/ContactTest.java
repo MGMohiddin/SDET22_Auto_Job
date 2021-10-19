2311683e05c559646b9cc5639acd28f26a4322f1
MD G MOHIDDIN <MD G MOHIDDIN@LAPTOP-R4KD4R46>
2021-10-19 15:26:27 +0530

    Second Commit

--- CRM_Maven_Project/src/test/java/com/crm/contactTest/ContactTest.java
+++ CRM_Maven_Project/src/test/java/com/crm/contactTest/ContactTest.java
@@ -20,5 +20,11 @@ public class ContactTest {
 	public void editContactTest() {
 		System.out.println("execute editContactTest");
 	}
+	
+	
+	@Test
+	public void deleteContactTest() {
+		System.out.println("execute deleteContactTest");
+	}
 
 }

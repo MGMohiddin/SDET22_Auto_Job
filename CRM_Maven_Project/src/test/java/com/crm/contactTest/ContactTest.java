1c7e3c2eace568da0d0ecb363165e59bbb0f54a3
MD G MOHIDDIN <MD G MOHIDDIN@LAPTOP-R4KD4R46>
2021-10-19 13:18:45 +0530

    First Commit

---
+++ CRM_Maven_Project/src/test/java/com/crm/contactTest/ContactTest.java
@@ -0,0 +1,24 @@
+package com.crm.contactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void createContactTest() {
+		
+		System.out.println("execute createContactTest... ");
+		
+		String URL = System.getProperty("url");
+		System.out.println(URL);
+	String BROWSER = System.getProperty("browser");
+	System.out.println(BROWSER);
+		}
+	
+	
+	@Test
+	public void editContactTest() {
+		System.out.println("execute editContactTest");
+	}
+
+}
